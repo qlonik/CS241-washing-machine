76d62d3a0b70731a493b7b2d200a6668f16b3d40
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-04-03 17:05:18 -0300

    Added comments

--- BINtoDual7Seg.v
+++ BINtoDual7Seg.v
@@ -1,5 +1,14 @@
 module BINtoDual7Seg(i, O);
 
+// Decode 4 bit binary number from range 0..15
+// to a dual 7 segment display signal.
+//
+// Input is a binary number with most significant
+// number on the left position.
+//
+// Output is a vector with first 7 bits to higher
+// order display, and second 7 to a second display.
+
 input [3:0] i;
 output [13:0] O;
 
@@ -16,7 +25,8 @@ assign
 	O[7]  = 0,
 	
 	O[6]  = (~i[2] & ~i[0]) | (i[2] & i[0]) | (i[3] & ~i[1]) | (~i[3] & i[1]),
-	O[5]  = (i[3] & ~i[1]) | (~i[1] & ~i[0]) | ~i[2] | (~i[3] & i[1] & i[0]) | (i[3] & ~i[0]),
+	O[5]  = (i[3] & ~i[1]) | (~i[1] & ~i[0]) | ~i[2] |
+		(~i[3] & i[1] & i[0]) | (i[3] & ~i[0]),
 	O[4]  = (~i[3] & ~i[1]) | i[0] | (~i[3] & i[2]) | (i[3] & i[1]) | (i[3] & ~i[2]),
 	O[3]  = (~i[2] & ~i[0]) | (i[3] & ~i[1]) | (i[2] & ~i[1] & i[0]) |
 		(i[3] & i[2] & i[0]) | (~i[3] & ~i[2] & i[1]) | (~i[3] & i[1] & ~i[0]),
@@ -25,6 +35,6 @@ assign
 		(i[3] & ~i[2] & ~i[1]) | (i[3] & i[2] & i[1]) | (i[2] & i[1] & ~i[0]) |
 		(i[3] & i[1] & ~i[0]),
 	O[0]  = (i[2] & ~i[1]) | (i[3] & ~i[1]) | (i[3] & i[2]) |
-	(i[2] & i[1] & ~i[0]) | (~i[3] & ~i[2] & i[1]);
+		(i[2] & i[1] & ~i[0]) | (~i[3] & ~i[2] & i[1]);
 	
 endmodule
