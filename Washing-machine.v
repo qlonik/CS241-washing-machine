1cce0f20565cbf1f0da9314fac75e7fc8db08604
thepenguin1140 <jason@wuertz.net>
2013-04-03 15:40:39 -0300

    inserted vars

--- Washing-machine.v
+++ Washing-machine.v
@@ -1,2 +1,6 @@
-module Washing-machine();
+module Washing-machine(start,clock,reset,cold,warm,hot,extra_rinse,full,empty,agitate,spin,pump,alert,cold_valve,hot_valve,timer_bus,stage_bus);
+
+	input start,clock,reset,cold,warm,hot,extra_rinse,full,empty;
+	output agitate,spin,pump,alert,cold_valve,hot_valve,timer_bus,stage_bus;
+	wire cold_override,wash_done,rinse-done,spin_done;
 endmodule
