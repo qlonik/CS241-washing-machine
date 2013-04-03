9c93e945b288c9864cb02120ac6594026d4b62b6
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-04-03 16:29:07 -0300

    Fixed module name according to top level entity

--- Washing_machine.v
+++ Washing_machine.v
@@ -1,6 +1,8 @@
-module Washing-machine(start,clock,reset,cold,warm,hot,extra_rinse,full,empty,agitate,spin,pump,alert,cold_valve,hot_valve,timer_bus,stage_bus);
+module Washing_machine(start,clock,reset,cold,warm,hot,extra_rinse,full,empty,
+	agitate,spin,pump,alert,cold_valve,hot_valve,timer_bus,stage_bus);
 
 	input start,clock,reset,cold,warm,hot,extra_rinse,full,empty;
 	output agitate,spin,pump,alert,cold_valve,hot_valve,timer_bus,stage_bus;
+	
 	wire cold_override,wash_done,rinse-done,spin_done;
 endmodule
