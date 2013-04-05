e6f72ceca71f524c7c2fa44ad823dfd52b94b237
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-04-04 22:45:13 -0300

    Fixed semicolons, formatting; added comment

--- MUX2to3.v
+++ MUX2to3.v
@@ -1,9 +1,14 @@
-module MUX2to3(bus_in,d0,d1,d2);
-	input[1:0] bus_in;
-	output d0,d1,d2;
+module MUX2to3(bus_in, d0, d1, d2);
+
+// Accepts 2bit bus with binary number of timer
+// and sets true to line that connected with that timer
+
+	input [1:0] bus_in;
+	output d0, d1, d2;
+	
 	assign
-		d0=~bus_in[1]&bus_in[0];
-		d1=bus_in[1]&~bus_in[0];
-		d2=bus_in[1]&bus_in[0];
+		d0 = ~bus_in[1] &  bus_in[0],
+		d1 =  bus_in[1] & ~bus_in[0],
+		d2 =  bus_in[1] &  bus_in[0];
 endmodule
 
