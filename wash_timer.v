786c04296677259e1828fb48ec38de9c9f8ecc48
thepenguin1140 <jason@wuertz.net>
2013-04-05 14:54:02 -0300

    added anable lines

--- wash_timer.v
+++ wash_timer.v
@@ -2,23 +2,28 @@ module wash_timer(CLOCK, reset, start, value, pause, active, done);
 
 input CLOCK, reset, start, pause;
 output active, done;
-output [3:0]value;	// provides the value (max 4 bits) of the
+output [3:0]value;	// provides the value1 (max 4 bits) of the
 							// current time on the timer – you’ll pass this on to a BCD decoder,
 							// and then to a 7-segment display if you want to see the timer count
-reg active, done;
-reg [3:0]value;
+reg active, done1;
+reg [3:0]value1;
 reg enabled;
-integer clockedValue;
-parameter defaultValue = 3;	// number of time units (seconds) this timer
+integer clockedvalue1;
+parameter defaultvalue1 = 3;	// number of time units (seconds) this timer
 										// will run
 parameter clockHZ = 50000000;	// we assume you’ll use the 50MHz clock feed
 										// if not, you’ll adjust this accordingly
+										
+tri_state_buffer(done1,done,active);
+tri_state_bus_buffer(value1,value,active);
+
+
 initial
 	begin
-		value = defaultValue;
+		value1 = defaultvalue1;
 		enabled = 0;
-		clockedValue = 0;
-		done = 0;
+		clockedvalue1 = 0;
+		done1 = 0;
 		active = 0;
 	end
 	
@@ -27,29 +32,29 @@ always @(negedge CLOCK)
 		active = enabled & ~pause;
 		if (reset)
 		begin
-			value = defaultValue;
+			value1 = defaultvalue1;
 			enabled = 0;
-			clockedValue = 0;
-			done = 0;
+			clockedvalue1 = 0;
+			done1 = 0;
 			active = 0;
 		end
 		if (enabled & ~pause)
-			clockedValue = clockedValue + 1;
-		if (clockedValue > clockHZ)
+			clockedvalue1 = clockedvalue1 + 1;
+		if (clockedvalue1 > clockHZ)
 		begin
-			value = value - 1;
-			clockedValue = 0;
+			value1 = value1 - 1;
+			clockedvalue1 = 0;
 		end
-		if (value == 0)
+		if (value1 == 0)
 		begin
 			enabled = 0;
-			done = 1;
+			done1 = 1;
 		end
-		if (start & ~enabled & ~done)
+		if (start & ~enabled & ~done1)
 		begin
-			done = 0;
+			done1 = 0;
 			enabled = 1;
-			value = defaultValue;
+			value1 = defaultvalue1;
 		end
 	end
 endmodule
