df793d7eca6c2c7c35c8c3059be74bc2a2bb14ba
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-04-05 00:53:33 -0300

    Fixed problems

--- valve_control.v
+++ valve_control.v
@@ -1,23 +1,27 @@
 module valve_control(bus_in,cold_override,bus_out);
-  input cold_override;
-	input[2:0] bus_in;
-	output[1:0] bus_out;
 
-/* bus_out[0] is cold valve
-   bus_out[1] is hot valve
-   bus_in[0] is cold select
-   bus_in[1] is warm select
-   bus_in[3] is hot select
+	input cold_override;
+	input [2:0] bus_in;
+	output [1:0] bus_out;
 
-   CR is cold override
-   C is cold
-   W is warm
-   H is hot
+/* 
+* bus_out[0] is cold valve
+* bus_out[1] is hot valve
+*
+* bus_in[0] is cold select
+* bus_in[1] is warm select
+* bus_in[2] is hot select
 */
 
+/*
 	assign 
-	bus_out[0] = cold_override + bus_out[0] + (bus_out[0] & bus_out[1]); 
-	bus_out[0] = ~cold_override & bus_in[3] & ~(bus_out[0] & bus_out[1]) + 						
-               ~cold_override & ~bus_in[0] & bus_out[0] & bus_out[1];
+		bus_out[0] = cold_override + bus_out[0] + (bus_out[0] & bus_out[1]); 
+		bus_out[0] = ~cold_override & bus_in[3] & ~(bus_out[0] & bus_out[1]) + 
+			~cold_override & ~bus_in[0] & bus_out[0] & bus_out[1];
+*/
+
+assign
+	bus_out[0] = cold_override | bus_in[0] | bus_in[1],
+	bus_out[1] = ~cold_override & (bus_in[1] | bus_in[2]);
 
 endmodule
