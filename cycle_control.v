bf983b41c7bfda2c702c5434a326df12b7253b75
thepenguin1140 <jason@wuertz.net>
2013-04-05 10:11:54 -0300

    completed code for rinse cycle

--- cycle_control.v
+++ cycle_control.v
@@ -1,9 +1,9 @@
-module cycle_control(bus_in,timer_done,stage_bus,output_control1,next,timer_select,valve_enable);
+module cycle_control(bus_in,timer_done,stage_bus,output_control1,next,timer_select,valve_enablei,cold_override);
 	input[4:0] bus_in;
 	input[3:0] stage_bus;
 	input timer_done;
 	output[3:0] output_control1;
-	output next,timer_select,valve_enable;
+	output next,timer_select,valve_enable,cold_override;
 	
 always@(posedge bus_in or posedge timer_done or posedge stage_bus)
 	case(stage_bus)
@@ -13,7 +13,7 @@ always@(posedge bus_in or posedge timer_done or posedge stage_bus)
 				valve_enable=1b'0; // disable valves
 			 	next=1b'1;
 				end
-		4b'0010: timer_select=2b'01;
+		4b'0010: timer_select=2b'01;//wash timer
 			 output_control1[3]=1b'1;//turn on agitator
 			 if(timer_done==1b'1)begin // if timer expires
 				output_control1[3]=1b'0; //turn of agitator
@@ -24,9 +24,23 @@ always@(posedge bus_in or posedge timer_done or posedge stage_bus)
 				output_control1[1]=1b'0; // turn off pump
 				next=1b'1;
 				end
-		4b'0100:
-		4b'0101:
-		4b'0110:
+		4b'0100: cold_override=1b'1;
+			valve_enable=1b'1;
+			if(bus_in[1])begin //if full
+				valve_enable=1b'0; //disable valves
+				next=1b'1;
+				end
+		4b'0101:timer_select=2b'10;//rinse timer
+			output_control1[3]=1b'1; //turn on agitator
+			if(timer_done==1b'1)begin //if timer expires
+				output_control1[3]=1b'0;
+				next=1b'1;
+				end
+		4b'0110:output_control1[1]=1b'1;
+			if(bus_in[0])begin
+				output_control1[1]=1b'0;
+				next=1b'1;
+				end
 		4b'0111:
 		4b'1000:
 		4b'1001:
