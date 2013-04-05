e5259baad402c3cf56c6c0e47c908729ec94e61a
ThePenguin1140 <jason@wuertz.net>
2013-04-04 23:01:14 -0300

    the last module yay

---
+++ cycle_control.v
@@ -0,0 +1,9 @@
+module cycle_control(bus_in,timer_done,stage_bus,output_control1,next,timer_select,valve_enable);
+	input[4:0] bus_in;
+	input[3:0] stage_bus;
+	input timer_done;
+	output[3:0] output_control1;
+	output next,timer_select,valve_enable;
+	
+endmodule
+
