795fe46ab9ea8f0be3f7d1157d1cfc5d3e894acc
thepenguin1140 <jason@wuertz.net>
2013-04-05 14:12:30 -0300

    fixed syntax errors

--- cycle_control.v
+++ cycle_control.v
@@ -8,106 +8,130 @@ module cycle_control(bus_in,timer_done,stage_bus,output_control1,next,timer_sele
 always@(posedge bus_in or posedge timer_done or posedge stage_bus)
 	case(stage_bus)
 		//IDLE
-		4b'0000:valve_enable=1b'0;
-			cold_override=1b'0;
-			timer_select=2b'00;
-			output_control=4b'0000;
+		4'b0000:begin 
+			valve_enable=1'b0;
+			cold_override=1'b0;
+			timer_select=2'b00;
+			output_control1=4'b0000;
 			next=bus_in[8];
+			end
 		//PREWASH	
-		4b'0001:next=1b'0;
-			valve_enable=1b'1; 
+		4'b0001:begin
+			next=1'b0;
+			valve_enable=1'b1;
 			if(bus_in[1]/*full*/) begin //if full
-				valve_enable=1b'0; // disable valves
-			 	next=1b'1;
+				valve_enable=1'b0; // disable valves
+			 	next=1'b1;
 				end
+			end
 		//WASH
-		4b'0010:next=1b'0;
-			timer_select=2b'01;//wash timer
-			output_control1[3]=1b'1;//turn on agitator
-			if(timer_done==1b'1)begin // if timer expires
-				output_control1[3]=1b'0; //turn of agitator
-				next=1b'1;
+		4'b0010:begin
+			next=1'b0;
+			timer_select=2'b01;//wash timer
+			output_control1[3]=1'b1;//turn on agitator
+			if(timer_done==1'b1)begin // if timer expires
+				output_control1[3]=1'b0; //turn of agitator
+				next=1'b1;
 				end
+			end
 		//POST WASH 
-		4b'0011:next=1b'0;
-			output_control1[1]=1b'1; //turn on pump
+		4'b0011:begin	
+			next=1'b0;
+			output_control1[1]=1'b1; //turn on pump
 			if(bus_in[0]/*empty*/)begin // if empty
-				output_control1[1]=1b'0; // turn off pump
-				next=1b'1;
+				output_control1[1]=1'b0; // turn off pump
+				next=1'b1;
 				end
+			end
 		//PRE RINSE
-		4b'0100:next=1b'0;
-			cold_override=1b'1;//turn on cold over ride
-			valve_enable=1b'1; //turn on valves
+		4'b0100:begin
+			next=1'b0;
+			cold_override=1'b1;//turn on cold over ride
+			valve_enable=1'b1;//turn on valves
 			if(bus_in[1])begin //if full
-				valve_enable=1b'0; //disable valves
-				next=1b'1;
+				valve_enable=1'b0; //disable valves
+				next=1'b1;
 				end
+			end
 		//RINSE 1
-		4b'0101:next=1b'0;
-			timer_select=2b'10;//rinse timer
-			output_control1[3]=1b'1; //turn on agitator
-			if(timer_done==1b'1)begin //if timer expires
-				output_control1[3]=1b'0;
-				next=1b'1;
+		4'b0101:begin
+			next=1'b0;
+			timer_select=2'b10;//rinse timer
+			output_control1[3]=1'b1; //turn on agitator
+			if(timer_done==1'b1)begin //if timer expires
+				output_control1[3]=1'b0;
+				next=1'b1;
 				end
+			end
 		//POST RINSE
-		4b'0110:next=1b'0;
-			output_control1[1]=1b'1;//turn on the pump
+		4'b0110:begin
+			next=1'b0;
+			output_control1[1]=1'b1;//turn on the pump
 			if(bus_in[0])begin // if empty
-				output_control1[1]=1b'0;//turn pump off
-				next=1b'1;
+				output_control1[1]=1'b0;//turn pump off
+				next=1'b1;
 				end
+			end
 		//PRE RINSE 
-		4b'0111:next=1b'0;
-			valve_enable=1b'1;//fill with water, cold_override is still active
+		4'b0111:begin
+			next=1'b0;
+			valve_enable=1'b1;//fill with water, cold_override is still active
 			if(bus_in[1])begin//if full
-				valve_enable=1b'0;
-				next=1b'1;
+				valve_enable=1'b0;
+				next=1'b1;
 				end
+			end
 		//RINSE 2
-		4b'1000:next=1b'0;
-			timer_select=2b'10;//rinse timer
-			output_control1[3]=1b'1;//turn on agitator
-			if(timer_done==1b'1)begin//if timer expires
-				output_control1[3]=1b'0;
-				next=1b'1;
+		4'b1000:begin
+			next=1'b0;
+			timer_select=2'b10;//rinse timer
+			output_control1[3]=1'b1;//turn on agitator
+			if(timer_done==1'b1)begin//if timer expires
+				output_control1[3]=1'b0;
+				next=1'b1;
 				end
+			end
 		//POST RINSE
-		4b'1001:next=1b'0;
-			output_control1[1]=1b'1;//turn on the pump
+		4'b1001:begin
+			next=1'b0;
+			output_control1[1]=1'b1;//turn on the pump
 			if(bus_in[0])begin//if empty
-				output_control1[1]=1b'0;//turn off pump
-				next=1b'1;
+				output_control1[1]=1'b0;//turn off pump
+				next=1'b1;
 				end
+			end
 		//PRE SPIN
-		4b'1010:next=1b'0;
-			output_control1[1]=1b'1;//turn on the pump
+		4'b1010:begin
+			next=1'b0;
+			output_control1[1]=1'b1;//turn on the pump
 			if(bus_in[0])begin//if empty
-				output_control1[1]=1b'0;//turn off pump
-				next=1b'1;
+				output_control1[1]=1'b0;//turn off pump
+				next=1'b1;
 				end
+			end
 		//SPIN
-		4b'1011:next=1b'0;
-			timer_select=2b'11;//spin timer start
-			output_control1[2]=1b'1;//turn spin on
-			if(timer_done==1b'1)begin//if timer expires
-				output_control1[2]=1b'0;
-				next=1b'1;
+		4'b1011:begin
+			next=1'b0;
+			timer_select=2'b11;//spin timer start
+			output_control1[2]=1'b1;//turn spin on
+			if(timer_done==1'b1)begin//if timer expires
+				output_control1[2]=1'b0;
+				next=1'b1;
 				end
+			end
 		//POST SPIN
-		4b'1100:next=1b'0;
-			output_control1[1]=1b'1;//turn on the pump
+		4'b1100:begin
+			next=1'b0;
+			output_control1[1]=1'b1;//turn on the pump
 			if(bus_in[0])begin//if empty
-				output_control1[1]=1b'0;//turn off pump
-				next=1b'1;
+				output_control1[1]=1'b0;//turn off pump
+				output_control1[0]=1'b1;//turn alarm on
+				next=1'b1;
 				end
-		//UNUSED
-		4b'1101:
-		4b'1110:
-		4b'1111:
+			end
+//		//UNUSED
+//		4'b1101:
+//		4'b1110:
+//		4'b1111:
 	endcase
-		
-
-
 endmodule
